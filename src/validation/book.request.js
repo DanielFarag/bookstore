238d880a45f013f9d6910f7ed55d18e30cbdc6e3
DanielFarag <danielfarag146@gmail.com>
2025-03-04 22:38:52 +0200

    chore: repush .env.example + remove request directory

---
+++ src/validation/book.request.js
@@ -0,0 +1,20 @@
+import Joi from 'joi';
+
+export default {
+    create:  Joi.object().keys({
+        title: Joi.string().required().min(3),
+        author: Joi.string().required().min(3),
+        price: Joi.number().required().min(0),
+        description: Joi.string().required().min(3),
+        stock: Joi.number().required().min(0),
+        image: Joi.any()
+    }),
+    update:  Joi.object().keys({
+        title: Joi.string().min(3),
+        author: Joi.string().min(3),
+        price: Joi.number().min(3),
+        description: Joi.string().min(3),
+        stock: Joi.number().min(3),
+        image: Joi.any()
+    })
+}
