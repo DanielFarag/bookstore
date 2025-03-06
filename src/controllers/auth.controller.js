2fc2e7f72badc7368548b300fbf82bdc3aed3211
DanielFarag <danielfarag146@gmail.com>
2025-03-06 16:13:02 +0200

    feature: define swagger json for auth + reviews

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -44,7 +44,7 @@ export const login = async (req, res) => {
       "+password"
     );
     if (!user || !user.isPasswordMatch(req.body.password)) {
-      res.status(401).json({ message: "Invalid Email or password" });
+      return res.status(401).json({ message: "Invalid Email or password" });
     }
 
     if (!user.isActive) {
--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -3,24 +3,27 @@ import Book from '../models/book.js';
 import validateReview from '../validation/reviewValidation.js';
 import { throwIfNotFound } from "../infrastructure/helpers/index.js";
 
-
 export const createReview = async (req, res) => {
     const { error } = validateReview(req.body);
+    
     if (error) return res.status(400).json({ error: error.details[0].message });
 
     
-        const book = await Book.findById(req.body.book);
-                if (!book) {
-                    throwIfNotFound(book);
-                }
-        const review = new Review({
-            user: req.user.id,
-            book: req.body.book,
-            rating: req.body.rating,
-            review: req.body.review
-        });
-        await review.save();
-        res.status(201).json(review);
+    const book = await Book.findById(req.body.book);
+    
+    if (!book) {
+        throwIfNotFound(book);
+    }
+
+    const review = new Review({
+        user: req.user.id,
+        book: req.body.book,
+        rating: req.body.rating,
+        review: req.body.review
+    });
+
+    await review.save();
+    res.status(201).json(review);
     
 };
 
@@ -31,38 +34,38 @@ export const getReviewsByBook = async (req, res) => {
     if (!book) {
         throwIfNotFound(book);
     }
-        const reviews = await Review.find({ book: req.params.bookId });
-        res.status(200).json(reviews);
-    
+    const reviews = await Review.find({ book: req.params.bookId });
+    res.status(200).json(reviews);
 };
 
 export const updateReview = async (req, res) => {
     const { error } = validateReview(req.body);
     if (error) return res.status(400).json({ error: error.details[0].message });
 
+    const review = await Review.findById(req.params.reviewId);
+    throwIfNotFound(review);
     
-        const review = await Review.findById(req.params.reviewId);
-        throwIfNotFound(review);
-        
-        if (review.user.toString() !== req.user.id.toString()) {
-            return res.status(403).json({ error: 'Unauthorized to update this review' });
-        }
+    if (review.user.toString() !== req.user.id.toString()) {
+        return res.status(403).json({ error: 'Unauthorized to update this review' });
+    }
 
-        review.rating = req.body.rating;
-        review.review = req.body.review;
-        await review.save();
-        res.status(200).json(review);
+    review.rating = req.body.rating;
+    review.review = req.body.review;
+    await review.save();
+    res.status(200).json(review);
     
 };
 
 export const deleteReview = async (req, res) => {
     
     const review = await Review.findById(req.params.reviewId);
-        throwIfNotFound(review);
-        
-        if (review.user.toString() !== req.user.id.toString()) {
-            return res.status(403).json({ error: 'Unauthorized to delete this review' });
-        }
+
+    throwIfNotFound(review);
+    
+    if (review.user.toString() !== req.user.id.toString()) {
+        return res.status(403).json({ error: 'Unauthorized to delete this review' });
+    }
+
     try{   
         await Review.deleteOne({ _id: req.params.reviewId });
         res.status(200).json({ message: 'Review deleted successfully' });
