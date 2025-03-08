5484aa19d381b6b28abee5d80b7734c1bd6d6e27
DanielFarag <danielfarag146@gmail.com>
2025-03-08 19:49:57 +0200

    chore: apply  filtration on book indexing + cache by filtration  criteria + reset cache on create/update/delete

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -2,113 +2,165 @@ import { BooksCollection, BookResource } from "../resources/index.js";
 import { Book } from "../models/index.js";
 import { throwIfNotFound } from "../infrastructure/helpers/index.js";
 import { redisClient } from "../config/redisClient.js";
+import { mailer } from "../infrastructure/services/index.js";
 
 export const index = async (req, res) => {
-  let { page, perPage } = req.query;
+  let {
+    page = 1,
+    perPage = 10,
+    title,
+    author,
+    description,
+    from_stock,
+    to_stock,
+    from_price,
+    to_price,
+  } = req.query
 
-  page = page ?? 1;
-  perPage = perPage ?? 10;
 
-  const cacheKey = `books:list:page:${page}:perPage:${perPage}`;
+  const queryParams = {
+    title,
+    author,
+    description,
+    from_stock,
+    to_stock,
+    from_price,
+    to_price,
+  };
 
-  try {
-    const cachedBooks = await redisClient.get(cacheKey);
-    if (cachedBooks) {
-      console.log("Cache hit for book list (index)");
-      return res.json(JSON.parse(cachedBooks));
-    }
+  const filteredParams = Object.entries(queryParams)
+    .filter(([_, value]) => value !== undefined)
+    .map(([key, value]) => `${key}:${value}`)
+    .join(':');
 
-    console.log("Cache miss for book list (index), fetching from DB");
-    const books = await Book.find()
-      .skip((page - 1) * perPage)
-      .limit(perPage);
-    const booksCollection = BooksCollection(books, req.query);
+  const cacheKey = `books:list:page:${page}:perPage:${perPage}${filteredParams ? ':' + filteredParams : ''}`;
 
-    const booksCollectionToCache = JSON.stringify(booksCollection);
-    await redisClient.set(cacheKey, booksCollectionToCache, { EX: 120 });
-    console.log("Book list (index) cached in Redis");
+  const cachedBooks = await redisClient.get(cacheKey);
 
-    return res.json(booksCollection);
-  } catch (error) {
-    console.error("Error in index endpoint:", error);
-    return res
-      .status(500)
-      .json({ message: "Failed to fetch books", error: error.message });
+  if (cachedBooks) {
+    console.log("Cache hit for book list (index)");
+    return res.json(JSON.parse(cachedBooks));
   }
+
+  page = parseInt(page) - 1
+  perPage = parseInt(perPage)
+
+  const filter={}
+  if (title) filter.title = new RegExp(title, 'i')
+  if (author) filter.author = new RegExp(author, 'i')
+  if (description) filter.description = new RegExp(description, 'i')
+    if (from_stock || to_stock) {
+      filter.stock = {}
+      if (from_stock) filter.stock.$gte = parseInt(from_stock)
+      if (to_stock) filter.stock.$lte = parseInt(to_stock)
+  }
+  if (from_price || to_price) {
+    filter.price = {}
+    if (from_price) filter.price.$gte = parseInt(from_price)
+    if (to_price) filter.price.$lte = parseInt(to_price)
+  }
+
+  const books = await Book.find(filter)
+    .skip(page * perPage)
+    .limit(perPage)
+
+
+  req.query.total = await Book.countDocuments(filter)
+    
+  const booksCollection = BooksCollection(books, req.query);
+
+  const booksCollectionToCache = JSON.stringify(booksCollection);
+  await redisClient.set(cacheKey, booksCollectionToCache, { EX: 120 });
+  console.log("Book list (index) cached in Redis");
+
+  return res.json(booksCollection);
 };
 
 export const show = async (req, res) => {
   const { id } = req.params;
   const cacheKey = `book:details:${id}`;
 
-  try {
-    const cachedBook = await redisClient.get(cacheKey);
-    if (cachedBook) {
-      console.log("Cache hit for book details (show)");
-      return res.json(JSON.parse(cachedBook));
-    }
-
-    console.log("Cache miss for book details (show), fetching from DB");
-    const book = await Book.findOne({ _id: id });
-    throwIfNotFound(book);
-    const bookResource = BookResource(book);
-
-    const bookResourceToCache = JSON.stringify(bookResource);
-    await redisClient.set(cacheKey, bookResourceToCache, { EX: 3600 });
-    console.log("Book details (show) cached in Redis");
-
-    return res.json(bookResource);
-  } catch (error) {
-    console.error("Error in show endpoint:", error);
-    if (error.name === "NotFoundError") {
-      return res.status(404).json({ message: error.message });
-    }
-    return res
-      .status(500)
-      .json({ message: "Failed to fetch book details", error: error.message });
+  const cachedBook = await redisClient.get(cacheKey);
+  if (cachedBook) {
+    console.log("Cache hit for book details (show)");
+    return res.json(JSON.parse(cachedBook));
   }
+
+  console.log("Cache miss for book details (show), fetching from DB");
+  const book = await Book.findOne({ _id: id });
+  throwIfNotFound(book);
+  const bookResource = BookResource(book);
+
+  const bookResourceToCache = JSON.stringify(bookResource);
+  await redisClient.set(cacheKey, bookResourceToCache, { EX: 3600 });
+  console.log("Book details (show) cached in Redis");
+
+  return res.json(bookResource);
 };
 
 export const store = async (req, res) => {
-  const { title, author, price, description, stock, image } = req.body;
+  if (!req.file) throw new Error('image_not_defined')
+
+  const { title, author, price, description, stock } = req.body
 
   const book = await Book.insertOne({
-    title,
-    author,
-    price,
-    description,
-    stock,
-    image,
-  });
-
-  const bookResource = BookResource(book);
-
-  res.status(201).json(bookResource);
-};
-
-export const update = async (req, res) => {
-  const { id } = req.params;
-
-  const { title, author, price, description, stock, image } = req.body;
-
-  const book = await Book.findByIdAndUpdate(
-    { _id: id },
-    {
       title,
       author,
       price,
       description,
       stock,
-      image,
-    },
-    { new: true }
-  );
+      image: req.file.filename,
+  })
 
-  throwIfNotFound(book);
+  const bookResource = BookResource(book)
 
-  const bookResource = BookResource(book);
+  mailer.notifyAllUsers(book)
 
-  res.status(201).json(bookResource);
+  const listCachePattern = `books:list:*`;
+  const keys = await redisClient.keys(listCachePattern);
+  if (keys.length > 0) {
+    await redisClient.del(...keys);
+  }
+
+
+  res.status(201).json(bookResource)
+    
+};
+
+export const update = async (req, res) => {
+  const { id } = req.params
+
+  const { title, author, price, description, stock } = req.body
+
+  const book = await Book.findByIdAndUpdate(
+      { _id: id },
+      {
+          title,
+          author,
+          price,
+          description,
+          stock,
+          image: req.file?.filename,
+      },
+      { new: true }
+  )
+
+  throwIfNotFound(book)
+
+  const bookResource = BookResource(book)
+
+
+  const cacheKey = `book:details:${id}`;
+  await redisClient.del(cacheKey);
+  
+  const listCachePattern = `books:list:*`;
+  const keys = await redisClient.keys(listCachePattern);
+  if (keys.length > 0) {
+    await redisClient.del(...keys);
+  }
+
+
+  res.status(201).json(bookResource)
 };
 
 export const remove = async (req, res) => {
@@ -116,9 +168,20 @@ export const remove = async (req, res) => {
 
   const book = await Book.findByIdAndDelete({ _id: id });
 
+  console.log(book)
   throwIfNotFound(book);
 
   const bookResource = BookResource(book);
+
+
+  const cacheKey = `book:details:${id}`;
+  await redisClient.del(cacheKey);
+  
+  const listCachePattern = `books:list:*`;
+  const keys = await redisClient.keys(listCachePattern);
+  if (keys.length > 0) {
+    await redisClient.del(...keys);
+  }
 
   res.json(bookResource);
 };
